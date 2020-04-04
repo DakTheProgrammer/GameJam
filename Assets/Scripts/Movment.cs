33444654e5af7db91d733d20abbf059688a20a85
DakTheProgrammer <dtw3200@live.com>
2020-04-03 19:42:44 -0500

    Moved position and finished movment

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
+    private Rigidbody Player;
+    public static float Speed = 400f;
+    private float SideForce = 1000f;
+    private float JumpForce = 10000f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Player.AddForce(Speed * Time.deltaTime, 0, 0); 
+        if(Input.GetKey("a"))
+        {
+            Player.AddForce(0, 0, SideForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            Player.AddForce(0, 0, -SideForce * Time.deltaTime);
+        }
+        if (Input.GetKey("space") && Player.position.y == 1)
+        {
+            Player.AddForce(0, JumpForce * Time.deltaTime, 0);
+        }
     }
 }
