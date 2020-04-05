ae63250224f1ae1a25e2137a3eab4edc38e41399
DakTheProgrammer <dtw3200@live.com>
2020-04-05 10:13:44 -0500

    edit

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -6,10 +6,12 @@ public class PlayerCollision : MonoBehaviour
 {
     private Movment move;
     public GameObject GameOverUI;
+    private Rigidbody rb;
 
     private void Start()
     {
         move = gameObject.GetComponent<Movment>();
+        rb = GameObject.Find("Player").GetComponent<Rigidbody>();
     }
     private void OnCollisionEnter(Collision collisionInfo)
     {
@@ -17,6 +19,10 @@ public class PlayerCollision : MonoBehaviour
         {
             move.enabled = false;
             GameOverUI.SetActive(true);
+            rb.AddForce(0, -100, 0);
+            rb.AddForce(0, -100, 0);
+            rb.AddForce(0, -100, 0);
+
         }
     }
 }
